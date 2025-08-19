535e56409c547683afe785243cb7dfbd9ab870e7
Ankitasinha21 <ankitasinhaha2108@gmail.com>
2025-08-19 11:28:38 +0530

    fhgfhgjhghg

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-const Live_url ='mongodb+srv://ankitasinhaha2108:1234Anki21@cluster0.uwyynth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const connectDB = async ()=>{
-    return mongoose.connect(process.env.Live_url)
+    return mongoose.connect(process.env.LIVE_URL)
 
     .then(()=>{
         console.log("database connection successful");
